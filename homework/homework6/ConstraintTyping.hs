35cde6eab41eda8bc7e6b824eea457a198fc3ab7
Taylor E. Berger <tberge01@cs.unm.edu>
2015-12-02 19:32:55 -0700

    prelim implementation for constraint typing. Going to rework to see if it can be done in a similar way to the book

--- homework/homework6/ConstraintTyping.hs
+++ homework/homework6/ConstraintTyping.hs
@@ -3,14 +3,18 @@ module ConstraintTyping where
 import qualified Syntax as S
 import qualified Unification as U
 
+import Control.Monad.State.Lazy
+import Data.List as L 
+
+type IdentifierTable = [] (S.Identifier, S.Type)
 type TypeConstraint = (S.Type, S.Type)
 type TypeConstraintSet = [] TypeConstraint
 type TypeSubstitution = []  (S.Identifier, S.Type)
 
-reconstructType :: S.Term -> Maybe S.Term
+--reconstructType :: S.Term -> Maybe S.Term
 reconstructType t = 
   let
-    constraints = deriveTypeConstraints t
+    constraints = evalState (deriveTypeConstraints t) (0, [])
     unifencoding = encode constraints
     (unifoutcome, unifsolvedequations) = U.unify unifencoding
   in
@@ -22,16 +26,86 @@ reconstructType t =
     U.HaltWithFailure -> Nothing
     U.HaltWithCycle -> Nothing
 
+-- TODO TODO
+applyTypeSubstitutionToTerm :: TypeSubstitution -> S.Term -> S.Term
+applyTypeSubstitutionToTerm _ t = t
+
+increment :: State (Integer, [] (S.Identifier, S.Type)) Integer
+increment = do
+  (i, ls) <- get
+  put ((i+1), ls)
+  return i 
+
+-- TODO TODO
+deriveTypeConstraints :: S.Term -> State (Integer, IdentifierTable) TypeConstraintSet
+--Zero
+deriveTypeConstraints S.Zero = do
+  i <- increment
+  return . return $ (S.TypeVar ('X':show i), S.TypeNat)
+--Tru 
+deriveTypeConstraints S.Tru = do
+  i <- increment
+  return . return $ (S.TypeVar ('X':show i), S.TypeBool)
+--Fls
+deriveTypeConstraints S.Fls = do
+  i <- increment
+  return . return $ (S.TypeVar ('X':show i), S.TypeBool) 
+--Succ t 
+deriveTypeConstraints (S.Succ t) = do
+  set <- deriveTypeConstraints t
+  i <- increment
+  return $ (S.TypeVar ('X':show i), S.TypeNat) :set
+--Pred t
+deriveTypeConstraints (S.Pred t) = do
+  set <- deriveTypeConstraints t
+  i <- increment
+  return $ (S.TypeVar ('X':show i), S.TypeNat) :set
+-- IsZero t 
+deriveTypeConstraints (S.IsZero t) = do
+  set <- deriveTypeConstraints t
+  i <- increment
+  return $ (S.TypeVar ('X':show i), S.TypeBool) :set
+-- If t1 t2 t3  
+deriveTypeConstraints (S.If t1 t2 t3) = do
+  o1@(s1:_) <- deriveTypeConstraints t1
+  o2@(s2:_) <- deriveTypeConstraints t2
+  o3@(s3:_) <- deriveTypeConstraints t3
+  return $ [(snd s2, snd s3), (snd s1, S.TypeBool)] ++ o1 ++ o2 ++ o3
+-- Abs id id_type t
+deriveTypeConstraints (S.Abs id id_type t) = do
+  (i, ls) <- get
+  put (i, (id, id_type):ls)
+  s@(s1:set) <- deriveTypeConstraints t
+  j <- increment
+  return $ (S.TypeVar ('X':show j), S.TypeArrow id_type (snd s1)) : set
+-- App t1 t2
+deriveTypeConstraints (S.App t1 t2) = do
+  o1@(s1:_) <- deriveTypeConstraints t1
+  o2@(s2:_) <- deriveTypeConstraints t2
+  i <- increment
+  return $ (snd s1, S.TypeArrow
+                      (snd s2)
+                      (S.TypeVar ('X':show i)))
+         : o1 ++ o2
+-- Fix t1 t2
+deriveTypeConstraints (S.Fix t) = do
+  o1@((_,S.TypeArrow f g):_) <- deriveTypeConstraints t
+  i <- increment
+  return $ (S.TypeVar ('X':show i), f) : o1
+-- Var id
+deriveTypeConstraints (S.Var id) = do
+  i <- increment
+  (_, ls) <- get
+  case L.lookup id ls of
+   Just a -> return $ return (S.TypeVar ('X':show i), a)
+   Nothing -> fail $ "Could not find identifier: " ++ id ++
+              "'s type. Perhaps an error in the source?"
+  
 type TypeUnifVar = S.Identifier
 data TypeUnifFun =
   TypeUnifArrow |
   TypeUnifBool |
   TypeUnifNat  deriving (Show, Eq)
-
-
--- TODO TODO TODO TODO 
-applyTypeSubstitutionToTerm = undefined
-deriveTypeConstraints = undefined
 
 encode :: TypeConstraintSet -> [U.Equation TypeUnifVar TypeUnifFun]
 encode = map (\(tau1, tau2) -> (enctype tau1, enctype tau2))
--- homework/homework6/Unification.hs
+++ homework/homework6/Unification.hs
@@ -1,6 +1,6 @@
 module Unification where
 
-import Data.List (lookup)
+import Data.List (find)
 
 data Term v f =
   Fun f [Term v f] |
@@ -20,8 +20,8 @@ type Substitution v f = [Binding v f]
 
 applySubst :: (Eq v, Eq f) => Term v f -> Substitution v f -> Term v f
 applySubst (Var x) theta =
-  case lookup x theta of
-   Just a -> a
+  case find (\(z,_) -> z == x) theta of
+   Just (_,a) -> a
    Nothing -> Var x 
 applySubst (Fun f tlist) theta =
   Fun f $ map (\x -> applySubst x theta) tlist
@@ -36,17 +36,19 @@ unify = unify' []
   where
     unify' :: (Eq v, Eq f) => [Equation v f] -> [Equation v f] -> (Outcome, [Equation v f])
     unify' sigma [] = (Success, sigma)
-    unify' sigma (theta:eqns) = case theta of
-      s@(Var v1, Var v2) -> if v1 == v2
-                            then unify' sigma eqns -- delete identity
-                            else unify' (s:sigma) $ applySubst' eqns [(v1, Var v2)]
-      s@(Var v, t) -> if inVars v t
-                      then (HaltWithCycle, [s])
-                      else unify' (s:sigma) $ applySubst' eqns [(v, t)]
-      (t, Var v) -> unify' sigma ((Var v, t):eqns) -- flip 
-      (Fun f terms_1, Fun g terms_2) -> if f == g
-                                        then unify' sigma $ (zip terms_1 terms_2) ++ eqns
-                                        else (HaltWithFailure, [theta])
+    unify' sigma (s@(Var v1, Var v2):eqns) = if  v1 == v2
+                                             then unify' sigma eqns -- delete identity
+                                             else unify' (s:sigma) $
+                                                    applySubst' eqns [(v1, Var v2)]
+    unify' sigma (s@(Var v, t):eqns) = if inVars v t
+                                        then (HaltWithCycle, [])
+                                        else unify' (s:sigma) $
+                                               applySubst' eqns [(v, t)]
+    unify' sigma ((t, Var v):eqns) = unify' sigma ((Var v, t):eqns) -- flip 
+    unify' sigma ((Fun f terms_1, Fun g terms_2):eqns) = if  f == g
+                                                         then unify' sigma $
+                                                                (zip terms_1 terms_2) ++ eqns
+                                                         else (HaltWithFailure, [])
 
 inVars :: Eq v => v -> Term v f -> Bool
 inVars v0 (Var v1) = v1 == v0
